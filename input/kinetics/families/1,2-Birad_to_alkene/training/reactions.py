a2e4b379f18a6136a08e4580b1f4a355c0bcf7ec
alongd <alongd@mit.edu>
2018-01-31 21:10:21 -0500

    fixup! Corrected training reaction in 1,2-Birad_to_alkene

--- input/kinetics/families/1,2-Birad_to_alkene/training/reactions.py
+++ input/kinetics/families/1,2-Birad_to_alkene/training/reactions.py
@@ -9,10 +9,11 @@ longDesc = u"""
 
 entry(
     index = 1,
-    label = "SO2(T) <=> SO2(S)",
+    label = "SO2(T) => SO2(S)",
     degeneracy = 1,
+    reversible = False,
     kinetics = Arrhenius(A=(1.0e+10, 's^-1'), n=0, Ea=(0, 'kcal/mol')),
-    rank = 3,
+    rank = 1,
     shortDesc = u"""""",
     longDesc =
 u"""
@@ -21,6 +22,5 @@ F.B. Wampler, K. Otsuka, J.G. Calvert, E.K. Damon, Int. J. Chem. Kin., 1973, 5(4
 and adjusted to a first order reaction at 1 atm
 Similar rates were determined by:
 T.N. Rao, S.S. Collier, J.G. Calvet, JACS, 1969, 91(7), 1616-1612, doi: 10.1021/ja01035a006
-F.B. Wampler, K. Otsuka, J.G. Calvert, E.K. Damon, Int. J. Chem. Kin., 1973, 5(4), 669-690, doi: 10.1002/kin.550050417
 """,
 )

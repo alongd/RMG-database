0cbbc1bb4d077ed3e1f0d5d6d390555de9d38cb5
alongd <alongd@mit.edu>
2018-02-23 15:44:34 -0500

    Renamed kinetic library H2S+S=2SH_1bar as HSSH_1bar
    and added 1 bar rates on the HSSH surface from
    C.R. Zhou, K. Sendt, B.S. Haynes, J. Phys. Chem. A, 2009, 113,
    8299-8306, doi: 10.1021/jp903185k

--- input/kinetics/libraries/Sulfur/HSSH_1bar/reactions.py
+++ input/kinetics/libraries/Sulfur/HSSH_1bar/reactions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-name = "Sulfur/H2S+S=2SH_1bar"
+name = "Sulfur/HSSH_1bar"
 shortDesc = u"H2S + S = SH + SH at 1bar"
 longDesc =u"""
 The H2S + S = SH + SH reaction has several pathways.
@@ -9,7 +9,7 @@ The direct H-abstraction on the triplet surface has a barrier of 45 kJ/mol
 H2S + S can also pass through an inter-system crossing (barrier estimated to be only 3 kJ/mol),
 passing through singlet H2SS and HSSH, yielding the same products (SH + SH).
 The direct H-abvstraction is significant above 800 K, and dominates above 1000 K.
-However, below 700-800 K this reaction is significantly dependent on pressure. 
+However, below 700-800 K this reaction is significantly dependent on pressure.
 This reaction should be modeled as two duplicate reactions - Arhennius and PDep.
 However, the PDep path is only givin in literature at 1 bar.
 This reaction behaves as P-Dep at low P, while its high-P limit is zero(!).
@@ -17,6 +17,7 @@ This library should not be used at higher pressures.
 
 [Marshall2011b] Y. Gao, C.(R) Z., K. Sendt, B.S. Haynes, P. Marshall, Proc. Comb. Inst., 2011, 33, 459-465, doi: 10.1016/j.proci.2010.05.020
 [Sendt2008] C.R. Zhou, K. Sendt, B.S. Haynes, J. Phys. Chem. A, 2008, 112, 3239-3247, doi: 10.1021/jp710488d
+[Sendt2009b] C.R. Zhou, K. Sendt, B.S. Haynes, J. Phys. Chem. A, 2009, 113, 8299-8306, doi: 10.1021/jp903185k
 """
 
 entry(
@@ -51,5 +52,56 @@ u"""
 DKCCSD(T)/cc-PVQZ_DK
 The H2S+S PES passes through an inter-system crossing with a low barrier, and is infact P-Dep at low T (below 800 K)
 The rate here is computed at 1 bar. Bath gas: Ar.
+""",
+)
+
+entry(
+    index = 3,
+    label = "HSS + H <=> SH + SH",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.63e+18, 'cm^3/(mol*s)'), n=-0.983, Ea=(1, 'kJ/mol'), T0=(1, 'K'),
+                         Tmin=(300, 'K'), Tmax=(2000, 'K')),
+    shortDesc = u"""[Sendt2009b]""",
+    longDesc =
+u"""
+Part of the "SOx" subset
+This reaction is on the same surface as the SH+SH case,
+it is given in this library since the rate constant was calculated for 1 bar only
+Table 2, -R1d
+calculations done at the MRCI/aug-cc-pV(Q+d)Z//CASSCF/cc-pVTZ level of theory
+""",
+)
+
+entry(
+    index = 4,
+    label = "HSS + H <=> H2S + S",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(4.19e+18, 'cm^3/(mol*s)'), n=-1.563, Ea=(2, 'kJ/mol'), T0=(1, 'K'),
+                         Tmin=(300, 'K'), Tmax=(2000, 'K')),
+    shortDesc = u"""[Sendt2009b]""",
+    longDesc =
+u"""
+Part of the "SOx" subset
+This reaction is on the same surface as the SH+SH case,
+it is given in this library since the rate constant was calculated for 1 bar only
+Table 2
+calculations done at the MRCI/aug-cc-pV(Q+d)Z//CASSCF/cc-pVTZ level of theory
+""",
+)
+
+entry(
+    index = 5,
+    label = "HSS + H <=> S2 + H2",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(2.91e+16, 'cm^3/(mol*s)'), n=-0.894, Ea=(0, 'kJ/mol'), T0=(1, 'K'),
+                         Tmin=(300, 'K'), Tmax=(2000, 'K')),
+    shortDesc = u"""[Sendt2009b]""",
+    longDesc =
+u"""
+Part of the "SOx" subset
+This reaction is on the same surface as the SH+SH case,
+it is given in this library since the rate constant was calculated for 1 bar only
+Table 2
+calculations done at the MRCI/aug-cc-pV(Q+d)Z//CASSCF/cc-pVTZ level of theory
 """,
 )

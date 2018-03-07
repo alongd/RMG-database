e5b5940fe45e0ad5680b1a3e718fd4a7132221a2
alongd <alongd@mit.edu>
2018-03-06 22:07:54 -0500

    TMP - 1bar

--- input/kinetics/libraries/Sulfur/HSSH_1bar/reactions.py
+++ input/kinetics/libraries/Sulfur/HSSH_1bar/reactions.py
@@ -4,7 +4,7 @@
 # rename to original
 
 name = "Sulfur/HSSH_1bar"
-shortDesc = u"H2S + S = SH + SH at 1bar"
+shortDesc = u"HSSH PES at 1bar"
 longDesc =u"""
 The H2S + S = SH + SH reaction has several pathways.
 The direct H-abstraction on the triplet surface has a barrier of 45 kJ/mol
@@ -23,38 +23,61 @@ This library should not be used at higher pressures.
 [Sendt2009b] C.R. Zhou, K. Sendt, B.S. Haynes, J. Phys. Chem. A, 2009, 113, 8299-8306, doi: 10.1021/jp903185k
 """
 
+# entry(
+#     index = 1,
+#     label = "H2S + S <=> SH + SH",
+#     degeneracy = 1,
+#     duplicate = True,
+#     kinetics = Arrhenius(A=(7.4e+06, 'cm^3/(mol*s)'), n=2.297, Ea=(9011, 'cal/mol'), T0=(1, 'K'),
+#                          Tmin=(300, 'K'), Tmax=(3000, 'K')),
+#     shortDesc = u"""[Sendt2008]""",
+#     longDesc =
+# u"""
+# k_abstraction, (R4a)
+# T range: 300-3000 K
+# calculations done at the MRCI/aug-cc-pV(Q+d)Z//MRCI/aug-cc-pVTZ level of theory
+#
+# (note that this source has a CORRECTION, doi: 10.1021/jp810800a, and the original rate should is multiplied by a factor
+# of x2: i.e., A = 7.4e+06 cm^3/(mol*s), NOT 3.7e+06 cm^3/(mol*s))
+# """,
+# )
+#
+# entry(
+#     index = 2,
+#     label = "H2S + S <=> SH + SH",
+#     degeneracy = 1,
+#     duplicate = True,
+#     kinetics = Arrhenius(A=(1.18e+18, 'cm^3/(mol*s)'), n=-1.685, Ea=(5975, 'cal/mol'), T0=(1, 'K'),
+#                          Tmin=(300, 'K'), Tmax=(1040, 'K')),
+#     shortDesc = u"""[Marshall2011b]""",
+#     longDesc =
+# u"""
+# DKCCSD(T)/cc-PVQZ_DK
+# The H2S+S PES passes through an inter-system crossing with a low barrier, and is in fact P-Dep at low T (below 800 K)
+# The rate here is computed at 1 bar. Bath gas: Ar.
+#
+# Also available from [Sendt2009b] R1c Table 2 (at different pressures in the Supporting Information)
+# """,
+# )
+
 entry(
     index = 1,
-    label = "H2S + S <=> SH + SH",
+    label = "HSS + H <=> S2 + H2",
     degeneracy = 1,
     duplicate = True,
-    kinetics = Arrhenius(A=(7.4e+06, 'cm^3/(mol*s)'), n=2.297, Ea=(9011, 'cal/mol'), T0=(1, 'K'),
-                         Tmin=(300, 'K'), Tmax=(3000, 'K')),
-    shortDesc = u"""[Sendt2008]""",
+    kinetics = MultiArrhenius(
+        arrhenius = [
+            Arrhenius(A=(2.91e+16, 'cm^3/(mol*s)'), n=-0.894, Ea=(0, 'kJ/mol'), T0=(1, 'K'), Tmin=(300, 'K'), Tmax=(2000, 'K')),
+            Arrhenius(A=(1.05e+08, 'cm^3/(mol*s)'), n=1.750, Ea=(-4, 'kJ/mol'), T0=(1, 'K'), Tmin=(300, 'K'), Tmax=(2000, 'K')),
+        ],
+    ),
+    shortDesc = u"""[Sendt2009b]""",
     longDesc =
 u"""
-k_abstraction, (R4a)
-T range: 300-3000 K
-calculations done at the MRCI/aug-cc-pV(Q+d)Z//MRCI/aug-cc-pVTZ level of theory
-
-(note that this source has a CORRECTION, doi: 10.1021/jp810800a, and the original rate should is multiplied by a factor
-of x2: i.e., A = 7.4e+06 cm^3/(mol*s), NOT 3.7e+06 cm^3/(mol*s))
-""",
-)
-
-entry(
-    index = 2,
-    label = "H2S + S <=> SH + SH",
-    degeneracy = 1,
-    duplicate = True,
-    kinetics = Arrhenius(A=(1.18e+18, 'cm^3/(mol*s)'), n=-1.685, Ea=(5975, 'cal/mol'), T0=(1, 'K'),
-                         Tmin=(300, 'K'), Tmax=(1040, 'K')),
-    shortDesc = u"""[Marshall2011b]""",
-    longDesc =
-u"""
-DKCCSD(T)/cc-PVQZ_DK
-The H2S+S PES passes through an inter-system crossing with a low barrier, and is in fact P-Dep at low T (below 800 K)
-The rate here is computed at 1 bar. Bath gas: Ar.
+R10
+calculations done at the MRCI/aug-cc-pV(Q+d)Z//CASSCF/cc-pVTZ level of theory
+One rate is not PDep, the other is PDep given at 1 bar
+also available from [Sendt2008] and [Marshall2011b]
 """,
 )
 

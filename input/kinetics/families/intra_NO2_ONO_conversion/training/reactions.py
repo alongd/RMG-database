6f9175f9ed810bed2e85518ca732328bcb1ff407
alongd <alongd@mit.edu>
2018-07-23 22:07:39 -0400

    Added C2H5NO2 <=> CH3CH2ONO as a training reaction
    to intra_NO2_ONO_conversion

--- input/kinetics/families/intra_NO2_ONO_conversion/training/reactions.py
+++ input/kinetics/families/intra_NO2_ONO_conversion/training/reactions.py
@@ -7,3 +7,11 @@ longDesc = u"""
 Put kinetic parameters for reactions to use as a training set for fitting
 group additivity values in this file.
 """
+
+entry(
+    index = 1,
+    label = "C2H5NO2 <=> CH3CH2ONO",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(2.1e+10, 's^-1'), n=1, Ea=(60660, 'cal/mol'), T0=(1, 'K')),
+    shortDesc = u"""From kinetic library NOx2018""",
+)
